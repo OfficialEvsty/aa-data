a96b6338d3c0e7be9f6b9ebc7a80dffc6090efc7
Daniil Evstigneev <officialevsty@MacBook-Pro-Daniil-2.local>
2025-07-26 14:18:06 +0300

    new serializable type

---
+++ domain/serializable/nicknames_with_conflicts.go
@@ -0,0 +1,61 @@
+package serializable
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+)
+
+type Point struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+func (s *Point) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("type assertion .([]byte) failed")
+	}
+	return json.Unmarshal(bytes, s)
+}
+
+func (s Point) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
+
+// Conflict similar nicknames under occurrence with box area
+type Conflict struct {
+	Similar []uuid.UUID `json:"similar"`
+	Box     [4]Point    `json:"box"`
+}
+
+func (s *Conflict) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("type assertion .([]byte) failed")
+	}
+	return json.Unmarshal(bytes, s)
+}
+
+func (s Conflict) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
+
+// NicknameResultWithConflicts result of nicknames recognition
+type NicknameResultWithConflicts struct {
+	Conflicts   []Conflict  `json:"conflicts"`
+	NicknameIDs []uuid.UUID `json:"nickname_ids"`
+}
+
+func (s *NicknameResultWithConflicts) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("type assertion .([]byte) failed")
+	}
+	return json.Unmarshal(bytes, s)
+}
+
+func (s NicknameResultWithConflicts) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
