7ab72da32d8640dcc8119f01f3ae80f87a8ed7f2
Daniil Evstigneev <officialevsty@MacBook-Pro-Daniil-2.local>
2025-07-28 22:46:45 +0300

    update query guild nicknames

--- queries/get_guild_nicknames_by_id.go
+++ queries/get_guild_nicknames_by_id.go
@@ -5,6 +5,7 @@ import (
 	db "github.com/OfficialEvsty/aa-data/db/interface"
 	"github.com/OfficialEvsty/aa-shared/golinq"
 	"github.com/google/uuid"
+	"github.com/lib/pq"
 	"time"
 )
 
@@ -37,7 +38,7 @@ func (q GetGuildNicknamesByIdQuery) Handle(ctx context.Context, guildIDs []uuid.
 			  JOIN aa_guilds AS g ON gn.guild_id = g.id
 			  JOIN aa_nicknames AS n ON gn.nickname_id = n.id
 			  WHERE gn.guild_id = ANY($1::uuid[])`
-	rows, err := q.exec.QueryContext(ctx, query, guildIDsStr)
+	rows, err := q.exec.QueryContext(ctx, query, pq.Array(guildIDsStr))
 	if err != nil {
 		return nil, err
 	}
