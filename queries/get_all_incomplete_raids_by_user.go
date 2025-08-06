cfbd4cf715540a9a97eb9c70547faa236526592b
Daniil Evstigneev <officialevsty@MacBook-Pro-Daniil-2.local>
2025-08-06 09:32:38 +0300

    added one more condition for user's incomplete raids while receiving them from db

--- queries/get_all_incomplete_raids_by_user.go
+++ queries/get_all_incomplete_raids_by_user.go
@@ -4,19 +4,9 @@ import (
 	"context"
 	"database/sql"
 	db "github.com/OfficialEvsty/aa-data/db/interface"
-	"github.com/OfficialEvsty/aa-data/domain/serializable"
+	"github.com/OfficialEvsty/aa-data/domain/usecase"
 	"github.com/google/uuid"
-	"time"
 )
-
-type IncompleteRaidDTO struct {
-	UserID    uuid.UUID           `json:"user_id"`
-	RaidID    uuid.UUID           `json:"raid_id"`
-	PublishID uuid.UUID           `json:"publish_id"`
-	Status    serializable.Status `json:"status"`
-	CreatedAt time.Time           `json:"created_at"`
-	RaidAt    *time.Time          `json:"raid_at"`
-}
 
 type GetAllIncompleteRaidsQuery struct {
 	exec db.ISqlExecutor
@@ -28,19 +18,19 @@ func NewGetAllIncompleteRaidQuery(exec db.ISqlExecutor) *GetAllIncompleteRaidsQu
 	}
 }
 
-func (q *GetAllIncompleteRaidsQuery) Handle(ctx context.Context, userID uuid.UUID) ([]*IncompleteRaidDTO, error) {
-	var incompleteRaids []*IncompleteRaidDTO
+func (q *GetAllIncompleteRaidsQuery) Handle(ctx context.Context, userID uuid.UUID) ([]*usecase.IncompleteRaidDTO, error) {
+	var incompleteRaids []*usecase.IncompleteRaidDTO
 	query := `SELECT tp.user_id, r.id, tp.publish_id, r.status, r.created_at, r.raid_at
 			  FROM raids AS r 
 			  JOIN tenant_publishes AS tp ON tp.publish_id = r.publish_id
-			  WHERE tp.user_id = $1`
+			  WHERE tp.user_id = $1 AND r.status <> 'resolved'`
 	rows, err := q.exec.QueryContext(ctx, query, userID)
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 	for rows.Next() {
-		var incompleteRaidDTO IncompleteRaidDTO
+		var incompleteRaidDTO usecase.IncompleteRaidDTO
 		err = rows.Scan(
 			&incompleteRaidDTO.UserID,
 			&incompleteRaidDTO.RaidID,
