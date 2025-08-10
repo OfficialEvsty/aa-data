e6dd50441829b96c2e50b8cd50ae279f674c42de
Daniil Evstigneev <officialevsty@MacBook-Pro-Daniil-2.local>
2025-08-10 13:55:47 +0300

    fix jsonb_set

--- queries/get_raid_nicknames_with_conflicts_info.go
+++ queries/get_raid_nicknames_with_conflicts_info.go
@@ -18,47 +18,45 @@ func NewGetRaidParticipantsInfoQuery(exec db.ISqlExecutor) *GetRaidParticipantsI
 func (q *GetRaidParticipantsInfoQuery) Handle(ctx context.Context, publishID uuid.UUID) (*usecase.RaidNicknamesAndConflictsWithS3Data, error) {
 	var dto usecase.RaidNicknamesAndConflictsWithS3Data
 	query := `SELECT
-				p.s3,
-				fp.result::jsonb
-					-- заменяем nickname_ids
-					|> jsonb_set(
-						'{nickname_ids}',
-						(
-							SELECT jsonb_agg(
-								jsonb_build_object(
-									'id', nick_id,
-									'name', n.name
-								)
-							)
-							FROM jsonb_array_elements_text(fp.result::jsonb->'nickname_ids') nick_id
-							LEFT JOIN aa_nicknames n ON n.id::text = nick_id
-						)
-					)
-					-- заменяем conflicts
-					|> jsonb_set(
-						'{conflicts}',
-						(
-							SELECT jsonb_agg(
-								jsonb_build_object(
-									'box', c->'box',
-									'similar', (
-										SELECT jsonb_agg(
-											jsonb_build_object(
-												'id', sim_id,
-												'name', n.name
-											)
-										)
-										FROM jsonb_array_elements_text(c->'similar') sim_id
-										LEFT JOIN aa_nicknames n ON n.id::text = sim_id
-									)
-								)
-							)
-							FROM jsonb_array_elements(fp.result::jsonb->'conflicts') c
-						)
-					) AS new_data
-			FROM finished_publishes fp
-            JOIN publishes p ON p.id = fp.publish_id
-            WHERE p.id=$1;`
+    p.s3,
+    jsonb_set(
+      jsonb_set(
+        fp.result::jsonb,
+        ARRAY['nickname_ids'],
+        (
+          SELECT jsonb_agg(
+            jsonb_build_object(
+              'id', nick_id,
+              'name', n.name
+            )
+          )
+          FROM jsonb_array_elements_text(fp.result::jsonb->'nickname_ids') nick_id
+          LEFT JOIN aa_nicknames n ON n.id::text = nick_id
+        )
+      ),
+      ARRAY['conflicts'],
+      (
+        SELECT jsonb_agg(
+          jsonb_build_object(
+            'box', c->'box',
+            'similar', (
+              SELECT jsonb_agg(
+                jsonb_build_object(
+                  'id', sim_id,
+                  'name', n.name
+                )
+              )
+              FROM jsonb_array_elements_text(c->'similar') sim_id
+              LEFT JOIN aa_nicknames n ON n.id::text = sim_id
+            )
+          )
+        )
+        FROM jsonb_array_elements(fp.result::jsonb->'conflicts') c
+      )
+    ) AS new_data
+FROM finished_publishes fp
+JOIN publishes p ON p.id = fp.publish_id
+WHERE p.id = $1;`
 	err := q.exec.QueryRowContext(
 		ctx,
 		query,
