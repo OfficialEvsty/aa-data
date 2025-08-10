4ef2aaec55b4d4a73c755c3620ead93131d3cb0b
Daniil Evstigneev <officialevsty@MacBook-Pro-Daniil-2.local>
2025-08-10 14:55:29 +0300

    fix jsonb_set

--- queries/get_raid_nicknames_with_conflicts_info.go
+++ queries/get_raid_nicknames_with_conflicts_info.go
@@ -23,39 +23,44 @@ func (q *GetRaidParticipantsInfoQuery) Handle(ctx context.Context, publishID uui
 				  jsonb_set(
 					fp.result::jsonb,
 					ARRAY['nickname_ids'],
-					(
-					  SELECT jsonb_agg(
-						jsonb_build_object(
-						  'id', nick_id,
-						  'name', n.name
-						)
-					  )
-					  FROM jsonb_array_elements_text(fp.result::jsonb->'nickname_ids') nick_id
-					  LEFT JOIN aa_nicknames n ON n.id::text = nick_id
-					  WHERE jsonb_typeof(fp.result::jsonb->'nickname_ids') = 'array'
-					)
-				  ),
-				  ARRAY['conflicts'],
-				  (
-					SELECT jsonb_agg(
-					  jsonb_build_object(
-						'box', c->'box',
-						'similar', (
+					CASE 
+					  WHEN jsonb_typeof(fp.result::jsonb->'nickname_ids') = 'array' THEN
+						(
 						  SELECT jsonb_agg(
 							jsonb_build_object(
-							  'id', sim_id,
+							  'id', nick_id,
 							  'name', n.name
 							)
 						  )
-						  FROM jsonb_array_elements_text(c->'similar') sim_id
-						  LEFT JOIN aa_nicknames n ON n.id::text = sim_id
-						  WHERE jsonb_typeof(c->'similar') = 'array'
+						  FROM jsonb_array_elements_text(fp.result::jsonb->'nickname_ids') nick_id
+						  LEFT JOIN aa_nicknames n ON n.id::text = nick_id
 						)
+					  ELSE '[]'::jsonb
+					END
+				  ),
+				  ARRAY['conflicts'],
+				  CASE
+					WHEN jsonb_typeof(fp.result::jsonb->'conflicts') = 'array' THEN
+					  (
+						SELECT jsonb_agg(
+						  jsonb_build_object(
+							'box', c->'box',
+							'similar', (
+							  SELECT jsonb_agg(
+								jsonb_build_object(
+								  'id', sim_id,
+								  'name', n.name
+								)
+							  )
+							  FROM jsonb_array_elements_text(c->'similar') sim_id
+							  LEFT JOIN aa_nicknames n ON n.id::text = sim_id
+							)
+						  )
+						)
+						FROM jsonb_array_elements(fp.result::jsonb->'conflicts') c
 					  )
-					)
-					FROM jsonb_array_elements(fp.result::jsonb->'conflicts') c
-					WHERE jsonb_typeof(fp.result::jsonb->'conflicts') = 'array'
-				  )
+					ELSE '[]'::jsonb
+				  END
 				) AS new_data
 			FROM finished_publishes fp
 			JOIN publishes p ON p.id = fp.publish_id
