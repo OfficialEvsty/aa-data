7691d3ab6e56f18452db18f1abedd4cbc3c65fbe
officialevsty <officialevsty@gmail.com>
2025-08-25 13:44:52 +0300

    added scan to serializables

--- domain/serializable/nicknames_with_conflicts_and_names.go
+++ domain/serializable/nicknames_with_conflicts_and_names.go
@@ -6,16 +6,37 @@ import (
 	"github.com/google/uuid"
 )
 
+type ParticipantConflict struct {
+	NicknameID uuid.UUID `json:"id"`
+	Name       string    `json:"name"`
+}
+
+func (s *ParticipantConflict) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("type assertion .([]byte) failed")
+	}
+	return json.Unmarshal(bytes, s)
+}
+
 type Participant struct {
 	NicknameID uuid.UUID `json:"id"`
 	Name       string    `json:"name"`
 	Box        [4]Point  `json:"box"`
 }
 
+func (s *Participant) Scan(src interface{}) error {
+	bytes, ok := src.([]byte)
+	if !ok {
+		return fmt.Errorf("type assertion .([]byte) failed")
+	}
+	return json.Unmarshal(bytes, s)
+}
+
 // NamedConflict similar nicknames under occurrence with box area
 type NamedConflict struct {
-	Similar []*Participant `json:"similar"`
-	Box     [4]Point       `json:"box"`
+	Similar []*ParticipantConflict `json:"similar"`
+	Box     [4]Point               `json:"box"`
 }
 
 func (s *NamedConflict) Scan(src interface{}) error {
