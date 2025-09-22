b06b90826fc29674b9f9c2b50b129a6a7c70d104
officialevsty <officialevsty@gmail.com>
2025-09-22 08:41:18 +0300

    changed get_chain_ids_by_nickname_ids_at_date_interval

--- queries/get_chain_ids_by_nickname_ids_at_date_interval.go
+++ queries/get_chain_ids_by_nickname_ids_at_date_interval.go
@@ -20,6 +20,7 @@ func (q *GetChainIdsByNicknameIdsAtDateIntervalQuery) Handle(
 	ctx context.Context,
 	nicknameIDs []uuid.UUID,
 	start time.Time,
+	end time.Time,
 ) (usecase.ChainedNicknames, error) {
 	query := `
 				WITH RECURSIVE root_chain AS (
@@ -47,14 +48,33 @@ func (q *GetChainIdsByNicknameIdsAtDateIntervalQuery) Handle(
 						rc.searched_nickname_id
 					FROM chains c
 					INNER JOIN root_chain rc ON rc.parent_chain_id = c.chain_id
+				),
+				intervals AS (
+					SELECT 
+						c.chain_id,
+						c.nickname_id,
+						c.parent_chain_id,
+						c.chained_at AS start_date,
+						COALESCE(child.chained_at, '9999-12-31'::timestamp) AS end_date,
+						c.active,
+						c.nickname_id AS searched_nickname_id
+					FROM chains c
+					LEFT JOIN chains child ON child.parent_chain_id = c.chain_id
 				)
 				SELECT 
-					chain_id AS root_chain_id,
-					searched_nickname_id
-				FROM root_chain
-				WHERE parent_chain_id IS NULL;
+					rc.chain_id AS root_chain_id,
+					rc.searched_nickname_id,
+					i.start_date,
+					i.end_date
+				FROM root_chain rc
+				JOIN intervals i ON i.chain_id = rc.chain_id
+				WHERE rc.parent_chain_id IS NULL
+				  AND i.start_date < i.end_date  -- базовое условие валидного интервала
+				  AND i.start_date < $3          -- пересечение с заданным интервалом [$3, $4]
+				  AND i.end_date   > $4;
+
 			`
-	rows, err := q.exec.QueryContext(ctx, query, nicknameIDs, start)
+	rows, err := q.exec.QueryContext(ctx, query, nicknameIDs, start, end)
 	if err != nil {
 		return nil, err
 	}
