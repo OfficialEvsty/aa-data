86f4b694699b745f80615bc7b5e577de3efcd5f4
officialevsty <officialevsty@gmail.com>
2025-09-22 21:18:35 +0300

    fix

--- queries/get_chain_ids_by_nickname_ids_at_date_interval.go
+++ queries/get_chain_ids_by_nickname_ids_at_date_interval.go
@@ -71,8 +71,8 @@ func (q *GetChainIdsByNicknameIdsAtDateIntervalQuery) Handle(
 				JOIN intervals i ON i.chain_id = rc.chain_id
 				WHERE rc.parent_chain_id IS NULL
 				  AND i.start_date < i.end_date  -- базовое условие валидного интервала
-				  AND i.start_date < $2          -- пересечение с заданным интервалом [$3, $4]
-				  AND i.end_date   > $3;
+				  AND i.start_date < $3          -- пересечение с заданным интервалом [$3, $4]
+				  AND i.end_date   > $2;
 
 			`
 	rows, err := q.exec.QueryContext(ctx, query, pq.Array(nicknameIDs), start, end)
