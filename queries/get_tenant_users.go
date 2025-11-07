0dab68b955ed69b1562347b1cc24899a3cfb4eaf
officialevsty <officialevsty@gmail.com>
2025-11-07 16:26:37 +0300

    added new query tenant users

---
+++ queries/get_tenant_users.go
@@ -0,0 +1,60 @@
+package queries
+
+import (
+	"context"
+	db "github.com/OfficialEvsty/aa-data/db/interface"
+	"github.com/google/uuid"
+	"time"
+)
+
+type TenantUser struct {
+	UserID   uuid.UUID `json:"user_id"`
+	TenantID uuid.UUID `json:"tenant_id"`
+	Username string    `json:"username"`
+	JoinedAt time.Time `json:"joined_at"`
+	Email    string    `json:"email"`
+	LastSeen time.Time `json:"last_seen"`
+}
+
+type GetTenantUserByTenantID struct {
+	exec db.ISqlExecutor
+}
+
+func NewGetTenantUserByTenantID(exec db.ISqlExecutor) *GetTenantUserByTenantID {
+	return &GetTenantUserByTenantID{
+		exec: exec,
+	}
+}
+
+func (q *GetTenantUserByTenantID) Handle(ctx context.Context, tenantID uuid.UUID) ([]*TenantUser, error) {
+	var res []*TenantUser
+	query := `SELECT u.id, tu.tenant_id, u.username, u.email, u.created_at, u.last_seen FROM tenant_users tu
+			  JOIN users as u ON u.id = tu.user_id
+			  WHERE tu.tenant_id = $1`
+	rows, err := q.exec.QueryContext(
+		ctx,
+		query,
+		tenantID,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var dto TenantUser
+	for rows.Next() {
+		err = rows.Scan(
+			&dto.UserID,
+			&dto.TenantID,
+			&dto.Username,
+			&dto.Email,
+			&dto.JoinedAt,
+			&dto.LastSeen,
+		)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, &dto)
+	}
+	return res, nil
+}
