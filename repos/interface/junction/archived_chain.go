9aefd5c82201c00c2d5376180a33faf0a5e188be
officialevsty <officialevsty@gmail.com>
2025-11-12 10:29:45 +0300

    added archived chains

--- repos/interface/junction/archived_chain.go
+++ repos/interface/junction/archived_chain.go
@@ -8,7 +8,7 @@ import (
 
 type IArchivedChainRepository interface {
 	Add(ctx context.Context, aChain domain.ArchivedChain) error
-	AddMany(ctx context.Context, aChains []domain.ArchivedChain)
+	AddMany(ctx context.Context, aChains []domain.ArchivedChain) error
 	List(ctx context.Context, tenantID uuid.UUID) ([]*domain.ArchivedChain, error)
 	IsArchived(ctx context.Context, chainID uuid.UUID) (bool, error)
 	Remove(ctx context.Context, chainID uuid.UUID) error
