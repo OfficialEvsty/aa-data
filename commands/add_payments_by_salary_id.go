d76245da5dec536c6957f75eb47e67db08b95ab0
officialevsty <officialevsty@gmail.com>
2025-11-14 11:27:00 +0300

    added logic when salary updates

--- commands/add_payments_by_salary_id.go
+++ commands/add_payments_by_salary_id.go
@@ -12,6 +12,7 @@ import (
 	junction_repos "github.com/OfficialEvsty/aa-data/repos/interface/junction"
 	junction_repos2 "github.com/OfficialEvsty/aa-data/repos/junction"
 	"github.com/google/uuid"
+	"math"
 )
 
 type AddPaymentsBySalaryIDCommand struct {
@@ -55,6 +56,25 @@ func (m *PaymentManager) Handle(
 		err = m.salaryRepo.WithTx(tx).Update(ctx, *salary)
 		if err != nil {
 			return err
+		}
+		query := `SELECT SUM(p.salary), s.fond, s.tax FROM payments p
+				  JOIN salaries s ON s.id = p.salary_id
+				  WHERE salary_id = $1
+				  GROUP BY s.fond, s.tax;`
+		var salarySum, fund, tax int
+
+		err = tx.QueryRowContext(ctx, query, command.SalaryID).Scan(&salarySum, &fund, &tax)
+		if err != nil {
+			return err
+		}
+		taxed := int(float32(fund) * (float32(tax) / 100))
+		fund -= taxed
+		query = `UPDATE salaries SET status=$2 WHERE id = $1`
+		if math.Abs(float64(fund-salarySum)) <= 1 {
+			_, err = tx.ExecContext(ctx, query, command.SalaryID, serializable.SalaryPaid)
+			if err != nil {
+				return err
+			}
 		}
 		return nil
 	})
